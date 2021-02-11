093a3bed0e4bc5f5f4260cf886aa429d93bdf12e
Aleksei Sidorin <sidorinaleksei@gmail.com>
2021-02-11 04:03:43 +0300

    add limits on coverege of branchs

--- src/main/java/ru/netology/domain/TShirt.java
+++ src/main/java/ru/netology/domain/TShirt.java
@@ -1,23 +1,17 @@
 package ru.netology.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.util.Objects;
 
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
 public class TShirt extends Product {
     private String color;
     private String size;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        TShirt shirt = (TShirt) o;
-        return Objects.equals(color, shirt.color) &&
-                Objects.equals(size, shirt.size);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), color, size);
-    }
 }
